f35359e4d2610a0a4b48c0a0297bb1d54b66166c
rodri0319 <57386040+rodri0319@users.noreply.github.com>
2023-11-03 19:21:19 -0600

    Add files via upload

---
+++ Practica6/Cliente/Cliente.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Cliente {
+    public static void main(String[] args) {
+        String serverAddress = "localhost"; // Cambia esto a la dirección del servidor si es diferente
+        int serverPort = 8888; // Cambia esto al puerto del servidor si es diferente
+
+        try {
+            Socket clientSocket = new Socket(serverAddress, serverPort);
+            DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+
+            // Menú para elegir entre cargar o descargar
+            System.out.println("1. Cargar Archivo");
+            System.out.println("2. Descargar Archivo");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Ingresa tu elección (1/2): ");
+            String choice = reader.readLine();
+
+            if (choice.equals("1")) {
+                // Cargar archivo
+                dos.writeUTF("UPLOAD");
+
+                System.out.print("Ingresa la ruta del archivo que deseas cargar: ");
+                String filePath = reader.readLine();
+                File file = new File(filePath);
+
+                if (file.exists() && file.isFile()) {
+                    dos.writeUTF(file.getName());
+                    dos.writeLong(file.length());
+
+                    try (FileInputStream fis = new FileInputStream(file)) {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = fis.read(buffer)) != -1) {
+                            dos.write(buffer, 0, bytesRead);
+                        }
+                        System.out.println("Archivo cargado exitosamente");
+                    }
+                } else {
+                    System.out.println("El archivo no existe o no es válido.");
+                }
+            } else if (choice.equals("2")) {
+                // Descargar archivo
+                dos.writeUTF("DOWNLOAD");
+
+                System.out.print("Ingresa el nombre del archivo que deseas descargar: ");
+                String fileName = reader.readLine();
+                dos.writeUTF(fileName);
+
+                long fileSize = dis.readLong();
+
+                if (fileSize > 0) {
+                    try (FileOutputStream fos = new FileOutputStream(fileName)) {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        long bytesReceived = 0;
+                        while (bytesReceived < fileSize && (bytesRead = dis.read(buffer)) != -1) {
+                            fos.write(buffer, 0, bytesRead);
+                            bytesReceived += bytesRead;
+                        }
+                        System.out.println("Archivo descargado exitosamente");
+                    }
+                } else {
+                    System.out.println("El archivo no existe en el servidor.");
+                }
+            } else {
+                System.out.println("Elección no válida.");
+            }
+
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Practica6/Servidor/Server.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.InetSocketAddress;
+
+public class Server {
+    public static void main(String[] args) {
+        int port = 8888; // Puerto del servidor
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("El servidor está escuchando en el puerto " + port);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Cliente conectado desde " + clientSocket.getInetAddress().getHostAddress());
+
+                // Configura un nuevo subproceso para manejar la solicitud del cliente
+                Thread clientHandler = new Thread(new ClientHandler(clientSocket));
+                clientHandler.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ClientHandler implements Runnable {
+    private Socket clientSocket;
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (
+            DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream())
+        ) {
+            String request = dis.readUTF();
+
+            if (request.equals("UPLOAD")) {
+                // Cargar un archivo al servidor
+                String fileName = dis.readUTF();
+                long fileSize = dis.readLong();
+                receiveFile(fileName, fileSize, dis);
+            } else if (request.equals("DOWNLOAD")) {
+                // Descargar un archivo desde el servidor
+                String fileName = dis.readUTF();
+                sendFile(fileName, dos);
+            } else {
+                System.out.println("Solicitud no válida: " + request);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void receiveFile(String fileName, long fileSize, DataInputStream dis) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(fileName)) {
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            long bytesReceived = 0;
+            while (bytesReceived < fileSize && (bytesRead = dis.read(buffer, 0, (int) Math.min(buffer.length, fileSize - bytesReceived))) != -1) {
+                fos.write(buffer, 0, bytesRead);
+                bytesReceived += bytesRead;
+            }
+            System.out.println("Archivo recibido: " + fileName);
+        }
+    }
+
+    private void sendFile(String fileName, DataOutputStream dos) throws IOException {
+        File file = new File(fileName);
+        if (file.exists() && file.isFile()) {
+            long fileSize = file.length();
+            dos.writeLong(fileSize);
+
+            try (FileInputStream fis = new FileInputStream(file)) {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = fis.read(buffer)) != -1) {
+                    dos.write(buffer, 0, bytesRead);
+                }
+                System.out.println("Archivo enviado: " + fileName);
+            }
+        } else {
+            dos.writeLong(0); // Envía un tamaño de archivo 0 para indicar que el archivo no existe
+            System.out.println("Archivo no encontrado: " + fileName);
+        }
+    }
+}
